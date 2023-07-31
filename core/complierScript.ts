32fdd20e5cf309853199a9cc0bb7403a53958494
kaijia <kaijia@kaijia.com>
2023-07-31 17:56:39 +0800

    解析字符串

--- core/complierScript.ts
+++ core/complierScript.ts
@@ -1,5 +1,6 @@
-const complierScript = () => {
+const complierScript = (content: string, isSetup: boolean = false) => {
   //
+  console.log(content, isSetup);
 };
 
 export default complierScript;
--- core/index.ts
+++ core/index.ts
@@ -1,5 +1,15 @@
+import { parseComponent } from "vue-template-compiler";
+import complierScript from "./complierScript";
+
 const startCompiler = (content: string) => {
   console.log(content);
+  const sfcDescriptor = parseComponent(content);
+  console.log("sfcDescriptor", sfcDescriptor);
+  // @ts-ignore
+  const { script, scriptSetup } = sfcDescriptor;
+  const scriptCode = script || scriptSetup?.content || "";
+  const isSetup = script ? false : scriptSetup.setup;
+  complierScript(scriptCode, isSetup);
 };
 
 export { startCompiler };
